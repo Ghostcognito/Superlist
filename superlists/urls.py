277416c70a9457ede1c92c9b60ce8539cad1c816
ghost <ghostcognito@gmail.com>
2015-12-15 18:52:12 +0000

    made the git repo

---
+++ superlists/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/', include('lists.urls')),
+    #url(r'^admin/', include(admin.site.urls)),
+)
+    
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+
